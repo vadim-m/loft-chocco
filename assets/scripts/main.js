160d4f7abd4dc7050733de74c4eaf0098b3c5313
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-05-05 15:41:52 +0300

    Написал код аккордеона секции menu-acco.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@
 const burgerBtn = document.querySelector("#burger-menu");
 const fullscreenMenu = document.querySelector(".fullscreen-menu");
 const teamList = document.querySelector(".team__list");
+const menuList = document.querySelector(".menu-acco__list");
 
 // Функционал Бургер-меню для планшетов и ниже
 //
@@ -22,23 +23,44 @@ let activeTeamItem = document.querySelector(".team__item");
 teamList.addEventListener("click", (e) => {
   // проверка, что мы попали именно на Заголовок с классом team__name
   if (e.target.classList.contains("team__name")) {
-    // Запишем его родителя - team__item в переменную
-    let listItem = e.target.parentNode.parentNode;
+    // Запишем его родителя (team__item) в переменную
+    let targetItem = e.target.parentNode.parentNode;
 
     // проверка есть ли уже у выбранного элемента team__item класс active
-    if (listItem.classList.contains("team__item--active")) {
+    if (targetItem.classList.contains("team__item--active")) {
       // если да, то удаляем его и выходим из функции!
-      listItem.classList.remove("team__item--active");
+      targetItem.classList.remove("team__item--active");
       return;
     } else if (activeTeamItem) {
       // если уже есть другой открытый элемент, то удадяем у него класс
       activeTeamItem.classList.remove("team__item--active");
     }
     // далее делаем активным айтемом тот, по которому кликнули
-    activeTeamItem = listItem;
+    activeTeamItem = targetItem;
     // и добавляем ему класс по таймауту, чтобы прошла анимации закрытия
     setTimeout(() => {
-      listItem.classList.add("team__item--active");
+      targetItem.classList.add("team__item--active");
     }, 450);
+  }
+});
+
+// Горизонтальный Аккордеон Меню
+//
+// Делаем первый айтем меню активным по умолчанию
+let activeMenuItem = document.querySelector(".menu-acco__item");
+
+menuList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("menu-acco__trigger")) {
+    let targetItem = e.target.parentNode;
+
+    if (targetItem.classList.contains("menu-acco__item--active")) {
+      targetItem.classList.remove("menu-acco__item--active");
+      return;
+    } else if (activeMenuItem) {
+      activeMenuItem.classList.remove("menu-acco__item--active");
+    }
+
+    activeMenuItem = targetItem;
+    activeMenuItem.classList.add("menu-acco__item--active");
   }
 });
